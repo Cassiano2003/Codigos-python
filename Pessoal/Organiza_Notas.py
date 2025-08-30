bc946bdd6a45ce8583dd07a313d7f31bf7cba792
Cassiano2003 <cassianocarvalhodesouza@gmail.com>
2025-08-29 23:15:50 -0400

    29-08-2025_23-15-50-Cassiano

--- Pessoal/Organiza_Notas.py
+++ Pessoal/Organiza_Notas.py
@@ -48,41 +48,12 @@ def Acha_posicao_nome(repetidos,nome_completo):
         dicio[nome_in] = lis
     return dicio
 
-'''def Acha_posicao_nomes_repetidos(nomes_completos):
-    dicio_posicoes = {}
-    
-    # Percorre a lista de nomes completos apenas uma vez
-    for i, nome in enumerate(nomes_completos):
-        # Se o nome já existe no dicionário, adicione a nova posição à lista
-        if nome in dicio_posicoes:
-            dicio_posicoes[nome].append(i)
-        # Se o nome não existe, crie uma nova chave com uma lista contendo a posição atual
-        else:
-            dicio_posicoes[nome] = [i]
-    
-    # Filtra o dicionário para manter apenas os nomes que aparecem mais de uma vez
-    repetidos_com_posicoes = {
-        nome: posicoes
-        for nome, posicoes in dicio_posicoes.items()
-        if len(posicoes) > 1
-    }
-    
-    return repetidos_com_posicoes'''
-
-
-def Retira_Mais_Um_Envio(arquivo):
-    nome_sobre = Cria_nome_e_sobre(arquivo=arquivo)
-    #print(arquivo.iloc[46])
-    repetidos = Verifica_nomes_repetidos(nomes_completos=nome_sobre)
-    #print(repetidos)
-    dicio = Acha_posicao_nome(repetidos=repetidos,nome_completo=nome_sobre)
-    #print(dicio)
-    
+def Valores_que_devem_ser_removidos(arquivo,repetidos,dicionario):
     menores_l = []
     for nome in repetidos:
         maior_l = -1
         maior = -1
-        for n,i in enumerate(dicio[nome]):
+        for n,i in enumerate(dicionario[nome]):
             print("Qual é: ",n)
             print(arquivo.iloc[i])
             num = float(arquivo.loc[i, 'Avaliar/10,0'].replace(',','.'))
@@ -96,8 +67,15 @@ def Retira_Mais_Um_Envio(arquivo):
                 maior_l = i
             else:
                 menores_l.append(i)
-    
-    arquivo = arquivo.drop(menores_l)
+    return menores_l
+
+
+def Retira_Mais_Um_Envio(arquivo):
+    nome_sobre = Cria_nome_e_sobre(arquivo=arquivo)
+    repetidos = Verifica_nomes_repetidos(nomes_completos=nome_sobre)
+    dicio = Acha_posicao_nome(repetidos=repetidos,nome_completo=nome_sobre)
+    remover = Valores_que_devem_ser_removidos(arquivo=arquivo,repetidos=repetidos,dicionario=dicio)
+    arquivo = arquivo.drop(remover)
     arquivo = arquivo.reset_index(drop=True)
 
     return arquivo
@@ -105,7 +83,7 @@ def Retira_Mais_Um_Envio(arquivo):
 
 
 def main():
-    #os.system("clear")
+    os.system("clear")
     arquivos = glob.glob('*.xlsx')
     for arquivo_nome in arquivos:
         arquivo = pd.read_excel(arquivo_nome)
