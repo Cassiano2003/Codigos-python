8941338b788571583ed448d3df13d579e60b1f6a
Cassiano2003 <cassianocarvalhodesouza@gmail.com>
2025-09-11 21:53:59 -0400

    11-09-2025_21-53-59-Cassiano

--- Pessoal/Organiza_Notas.py
+++ Pessoal/Organiza_Notas.py
@@ -3,11 +3,14 @@ import glob
 import os
 
 class Aluno:
-    def __init__(self,Email,Primeiro_nome,Segundo_Nome):
+    def __init__(self,Email,Primeiro_nome,Segundo_Nome,Nota_Atividade):
         self.Email = Email #Minha key
         self.Primeiro_nome = Primeiro_nome
         self.Segundo_nome = Segundo_Nome
-        self.Nota_Final = []
+        self.Nota_Final = [] #Vai ser usada para poder gerar a media
+        self.Nota = 0
+        self.Nota_Atividade = Nota_Atividade
+        self.Nota_Exercicios = [] #Lista das notas da quele .xlsx
         self.Media_Final = 0
     
     def Calcula_Media(self,div):
@@ -17,96 +20,83 @@ class Aluno:
         self.Media_Final = soma/div
 
 
-    def Adiciona_Notas(self,nota):
+    def Adiciona_Nota(self,nota):
+        self.Nota = nota
+
+    def Adiciona_Notas_Exercicios(self,nota):
+        self.Nota_Exercicios.append(nota)
+
+    def Adiciona_Notas_Final(self,nota):
         self.Nota_Final.append(nota)
     
     def __str__(self):
-        return f"{self.Primeiro_nome} {self.Segundo_nome} e suas notas é {self.Nota_Final} e a media desse aluno é {self.Media_Final}"
+        return f"{self.Primeiro_nome} {self.Segundo_nome} e suas notas é {self.Nota_Exercicios},notas de atividades {self.Nota_Atividade} e sua nota final é {self.Nota}\nAs notas finais{self.Nota_Final} e a media desse aluno é {self.Media_Final}"
 
-def Colocar_Nota_10_para_4(arquivo):
-    valor = (arquivo['Avaliar/10,0'])
-    arquivo['Nota Final'] = arquivo['Nota Final'].astype(str)
-    for i,n in enumerate(valor):
-        novos_valores = (n.replace(',','.'))
-        if len(valor)-1 > i:
-            if float(novos_valores) >= 4:
-                arquivo.loc[i,'Nota Final'] = '10,0'
+def Menores_Notas(repetidos,alunos,arquivo):
+    menores = []
+    for emais in repetidos:
+        maior = -1
+        local_maior = -1
+        locais = Busca_Posicoes(emais,alunos)
+        for local in locais:
+            if alunos[local].Nota_Atividade > maior:
+                print(alunos[local].Nota_Atividade)
+                maior = alunos[local].Nota_Atividade
+                if local_maior != -1:
+                    menores.append(local_maior)
+                local_maior = local
             else:
-                arquivo.loc[i,'Nota Final'] = arquivo.loc[i,'Avaliar/10,0']
-        else:
-            arquivo.loc[i,'Nota Final'] = ''
-    return arquivo
+                menores.append(local)
+    return menores
 
-def Cria_nome_e_sobre(arquivo):
-    nomes = arquivo['Nome']
-    sobre_nome = arquivo['Sobrenome']
-    nome_sobre = []
-    for i, nome in enumerate(nomes):
-        if len(sobre_nome)-1 > i:
-            nome_sobre.append(nome+' '+sobre_nome[i])
-    return nome,sobre_nome,nome_sobre
+def Busca_Posicoes(email,alunos):
+    posicoes = []
+    for i,aluno in enumerate(alunos):
+        if aluno.Email == email:
+            posicoes.append(i)
+    return posicoes
 
-def Verifica_nomes_repetidos(nomes_completos):
+def Alunos_Repetidos(alunos):
     contagem = {}
     repetidos = []
-    for nome in nomes_completos:
-        if nome in contagem:
-            contagem[nome] += 1
+    for aluno in alunos:
+        if aluno.Email in contagem:
+            contagem[aluno.Email] += 1
         else:
-            contagem[nome] = 1
-    for nome, cont in contagem.items():
+            contagem[aluno.Email] = 1
+    
+    for email, cont in contagem.items():
         if cont > 1:
-            repetidos.append(nome)
+            repetidos.append(email)
     return repetidos
 
-def Acha_posicao_nome(repetidos,nome_completo):
-    dicio = {}
-    for nome_in in repetidos:
-        lis = []
-        for i,nome_sb in enumerate(nome_completo):
-            if nome_in == nome_sb:
-                lis.append(i)
-        dicio[nome_in] = lis
-    return dicio
-
-def Valores_que_devem_ser_removidos(arquivo,repetidos,dicionario):
-    menores_l = []
-    for nome in repetidos:
-        maior_l = -1
-        maior = -1
-        for n,i in enumerate(dicionario[nome]):
-            print("Qual é: ",n)
-            print(arquivo.iloc[i])
-            num = float(arquivo.loc[i, 'Avaliar/10,0'].replace(',','.'))
-            print("Meu numero é: ",num)
-            if num > maior:
-                print("Maior antes: ",maior)
-                maior = num
-                print("Maior depois: ",maior)
-                if maior_l != -1:
-                    menores_l.append(maior_l)    
-                maior_l = i
-            else:
-                menores_l.append(i)
-    return menores_l
-
-
-def Retira_Mais_Um_Envio(arquivo):
-    nome,sobre_nome,nome_sobre = Cria_nome_e_sobre(arquivo=arquivo)
-    repetidos = Verifica_nomes_repetidos(nomes_completos=nome_sobre)
-    dicio = Acha_posicao_nome(repetidos=repetidos,nome_completo=nome_sobre)
-    remover = Valores_que_devem_ser_removidos(arquivo=arquivo,repetidos=repetidos,dicionario=dicio)
-    arquivo = arquivo.drop(remover)
-    arquivo = arquivo.reset_index(drop=True)
-
+def Coloca_Notas(alunos,arquivo):
+    for i,aluno in enumerate(alunos):
+        arquivo.loc[i, 'Nota Final'] = round(aluno.Nota, 2)
     return arquivo
+
+def Calcula_Nota_Final(aluno,nota_corte):
+    quantas_notas = 0
+    for nota in aluno.Nota_Exercicios:
+        if nota >= nota_corte:
+            quantas_notas += 1
+    if quantas_notas >= 3:
+        aluno.Adiciona_Nota(10.0)
+    else:
+        aluno.Adiciona_Nota(quantas_notas*(3.333333))
+
+def Adiciona_notas(i,arquivo,ate,aluno):
+    primeira_linha = arquivo.iloc[i]
+    for j in range(8,ate):
+        aluno.Adiciona_Notas_Exercicios(float(str(primeira_linha[j]).replace(",", ".")))
 
 def Pega_informacoes(arquivo):
     nomes = arquivo['Nome']
     sobre = arquivo['Sobrenome']
     email = arquivo['Endereço de email']
     nota_final = arquivo['Nota Final']
-    return email,nomes,sobre,nota_final
+    nota_atividade = [float((ati).replace(",", ".")) for ati in arquivo['Avaliar/10,0']]
+    return email,nomes,sobre,nota_final,nota_atividade
 
 def Busca_Aluno(email,list_alunos):
     for i,aluno in enumerate(list_alunos):
@@ -118,10 +108,32 @@ def Imprime_Alunos(alunos):
     for aluno in alunos:
         print(aluno)
 
-
 def Gera_dados(alunos):
-    dados = {"Sobrenome": None,	"Nome":None,"Media Final":None}
+    dados = {"Sobrenome": [],	"Nome":[],"Endereço de email": [],"Media Final":[]}
+    for aluno in alunos:
+        dados["Nome"].append(aluno.Primeiro_nome)
+        dados["Sobrenome"].append(aluno.Segundo_nome)
+        dados["Endereço de email"].append(aluno.Email)
+        dados["Media Final"].append(round(aluno.Media_Final, 2))
+    df = pd.DataFrame(dados)
+    print(df)
+    nome_arquivo = input("Digite o nome do arquivo: ")
+    nome_arquivo = nome_arquivo+".xlsx"
+    df.to_excel(nome_arquivo,index=False)
     
+def Numeros_Validos(arquivos):
+    for i,ar in enumerate(arquivos):
+        print("[",i,"]",ar)
+    print("------"*10)
+    minhas_escolhas = input("Digite os numeros desejados separados por espaço: ")
+    numros_escolhas = [int(es) for es in minhas_escolhas.split()]
+    print("------"*10)
+    for num in numros_escolhas:
+        if num > len(arquivos)-1:
+            print("Numeros invalidos!!!")
+            return Numeros_Validos(arquivos)
+    print("Numeros validos!!!")
+    return numros_escolhas     
 
 def menu():
     os.system("clear")
@@ -130,55 +142,76 @@ def menu():
     print("------"*10)
     print("Selecione os arquivos que queiras manipular.")
     print("------"*10)
-    for i,ar in enumerate(arquivos):
-        print("[",i,"]",ar)
-    print("------"*10)
-    minhas_escolhas = input("Digite os numeros desejados separados por espaço: ")
-    numros_escolhas = [int(es) for es in minhas_escolhas.split()]
-    div = len(numros_escolhas)
+    numros_escolhas = Numeros_Validos(arquivos)
     print("------"*10)
     for op in opcoes:
         print(op)
     print("------"*10)
     escolha = int(input("Escolha qual sera a operação desejada: "))
-    alunos = []
     match escolha:
         case 0:
+            #Lendo o arquivo
             for num in numros_escolhas:
+                print("------"*10)
+                os.system("clear")
+                print("Arquivo selecionado ",arquivos[num])
+                print("------"*10)
+                alunos = []
                 arquivo = pd.read_excel(arquivos[num])
                 if 'Nota Final'in arquivo.columns:
                     print("A coluna Nota final ja existe.")
                 else:
                     numero_colunas = len(arquivo.iloc[0])
                     arquivo.insert(numero_colunas,'Nota Final',0)
-                    arquivo = Colocar_Nota_10_para_4(arquivo)
-                    arquivo = Retira_Mais_Um_Envio(arquivo)
-                    arquivo.to_excel(arquivos[num], index=False) # index=False para não incluir o índice do DataFrame no arquivo
+                #Gero o vetor da Classe Alunos
+                email,nome,sobre,nota_final,nota_atividade = Pega_informacoes(arquivo=arquivo)
+                ques = int(input("Quantas questoes tem: "))
+                for i in range(len(arquivo)):
+                    aluno = Aluno(email[i],nome[i],sobre[i],nota_atividade[i])
+                    Adiciona_notas(i,arquivo,8+ques,aluno)
+                    alunos.append(aluno)
+                #Vai gerar as notas de cada aluno
+                nota_corte = float(input("Digite a nota de corte: "))
+                for aluno in alunos:
+                    Calcula_Nota_Final(aluno,nota_corte)
+                #Coloca as notas de cada aluno na planilha
+                alunos.pop(len(alunos)-1)
+                arquivo = Coloca_Notas(alunos,arquivo)
+                repetidos = Alunos_Repetidos(alunos)
+                menores = Menores_Notas(repetidos,alunos,arquivo)
+                print(menores)
+                arquivo = arquivo.drop(menores)
+                arquivo = arquivo.reset_index(drop=True)
+                #Imprime_Alunos(alunos)
+                arquivo.to_excel(arquivos[num], index=False) # index=False para não incluir o índice do DataFrame no arquivo
+                input("Precione entter para continuar")
         case 1:
+            alunos = []
             for num in numros_escolhas:
                 arquivo = pd.read_excel(arquivos[num])
-                email,nome,sobre,nota_final = Pega_informacoes(arquivo=arquivo)
+                email,nome,sobre,nota_final,nota_atividade = Pega_informacoes(arquivo=arquivo)
                 if len(alunos) == 0:
                     em_tam, nm_tam, sb_tam, nf_tam = len(email),len(nome),len(sobre),len(nota_final)
                     if(em_tam == nm_tam == sb_tam == nf_tam):
                         for i in range(em_tam):
                             nova_nota = float(str(nota_final[i]).replace(",", "."))
-                            aluno = Aluno(email[i],nome[i],sobre[i])
-                            aluno.Adiciona_Notas(nova_nota)
+                            aluno = Aluno(email[i],nome[i],sobre[i],nota_atividade[i])
+                            aluno.Adiciona_Notas_Final(nova_nota)
                             alunos.append(aluno)
                 elif len(alunos) > 0:
                     for i,em in enumerate(email):
                         lugar = Busca_Aluno(em,alunos)
                         nova_nota = float(str(nota_final[i]).replace(",", "."))
                         if lugar != -1:
-                            alunos[lugar].Adiciona_Notas(nova_nota)
+                            alunos[lugar].Adiciona_Notas_Final(nova_nota)
             for aluno in alunos:
                 aluno.Calcula_Media(len(numros_escolhas))
             alunos.pop(len(alunos)-1)
-            Imprime_Alunos(alunos)
+            #Imprime_Alunos(alunos)
+            Gera_dados(alunos)
         case _:
             print("Não tem essa operação\nPrograma finalizado!!!")
             return
-    
+    print("------"*10)
 
 menu()
